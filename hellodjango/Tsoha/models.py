f6dc4835f5304f5a981b55347df3900ff182daf1
myztinen <myztinen@gmail.com>
2011-11-05 14:05:01 +0200

    added test model and view

--- hellodjango/Tsoha/models.py
+++ hellodjango/Tsoha/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Drink(models.Model):
+    drinkType = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.drinkType
 # Create your models here.
