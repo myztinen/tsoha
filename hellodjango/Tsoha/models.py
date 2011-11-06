2bea8c41838706d23ec31a62285cdd595cf4569a
myztinen <myztinen@gmail.com>
2011-11-06 20:34:30 +0200

    added initial models

--- hellodjango/Tsoha/models.py
+++ hellodjango/Tsoha/models.py
@@ -5,4 +5,24 @@ class Drink(models.Model):
     
     def __unicode__(self):
         return self.drink_type
+        
+class Drink_name(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.name
+    
+class DrinkToName(models.Model):
+    drink_id = models.ForeignKey(Drink)
+    drink_name_id = models.ForeignKey(Drink_name)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.name
+    
+class DrinkToIngredient(models.Model):
+    drink_id = models.ForeignKey(Drink)
+    ingredient_id = models.ForeignKey(Ingredient)
 # Create your models here.
