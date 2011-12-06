7ab7a8c9223a05ad2f96a8e06a2aa147555a1100
myztinen <myztinen@gmail.com>
2011-12-06 16:09:17 +0200

    trying to get the css working

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -57,11 +57,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://pure-robot-5569.herokuapp.com/static/'
+STATIC_URL = 'http://pure-robot-5569.herokuapp.com/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
